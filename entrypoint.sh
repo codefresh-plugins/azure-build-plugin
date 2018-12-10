c0587956088d4a08364a9c1ab3b1e6ce3a51397c
amir-codefresh <33693628+amir-codefresh@users.noreply.github.com>
2018-12-10 17:23:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ az login --service-principal -u $APP_ID -p $P --tenant $TENANT >/dev/null
 
 az acr build --registry $ACR_NAME --image $IMAGE:$TAG --file ${DOCKERFILE_PATH:-Dockerfile} $CF_VOLUME_PATH/$CF_REPO_NAME/
 
-echo AZURE_IMAGE=$ACR_NAME.azureecr.io/$IMAGE:$TAG >> ${{CF_VOLUME_PATH}}/env_vars_to_export
+echo AZURE_IMAGE=$ACR_NAME.azureecr.io/$IMAGE:$TAG >> $CF_VOLUME_PATH/env_vars_to_export
