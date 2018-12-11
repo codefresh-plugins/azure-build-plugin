563737a4f5c6af9af29c54b0f3f4f346f669dbdb
amir-codefresh <33693628+amir-codefresh@users.noreply.github.com>
2018-12-11 09:03:56 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 msg() { echo -e "INF---> $1"; }
 err() { echo -e "ERR---> $1" ; exit 1; }
 
-az login --service-principal -u $APP_ID -p $P --tenant $TENANT >/dev/null
+az login --service-principal -u $APP_ID -p $PASSWORD --tenant $TENANT >/dev/null
 
 az acr build --registry $ACR_NAME --image $IMAGE:$TAG --file ${DOCKERFILE_PATH:-Dockerfile} $CF_VOLUME_PATH/$CF_REPO_NAME/
 
